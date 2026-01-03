157d6cfec6164987188dad96d3cdedb3fe16963c
Espy <232303947+Espy-Tech@users.noreply.github.com>
2026-01-03 21:48:46 +0300

    Update Projet_de_devinette.cpp

--- Projet_de_devinette.cpp
+++ Projet_de_devinette.cpp
@@ -1,73 +1,68 @@
-﻿#include<iostream>
-#include<cstdlib>	//for srand et rand
-#include<ctime>		//time
+#include <iostream>
+#include <cstdlib>    // for srand et rand
+#include <ctime>      // time
 
 using namespace std;
+
 int main() {
-	
-	srand(time(0));		//current time
-	int nombreDevine = rand() % 101;	//in 0 to 100
-	int nbrUser;
-	int vie, choix;
+    srand(time(0));        // current time
+    int nombreDevine = rand() % 101;    // in 0 to 100
+    int nbrUser;
+    int vie, choix;
 
+    cout << "Salut bienvenue dans le jeu de devinette des nombres generer par l'ordinateur\nVeuillez selectioner une des options: " << endl;
+    cout << "\n1. Niveau facile (10 vies)\n2. Intermediaire (5 vies)\n3. Dur (3 vies)\n";
 
-	cout << "Salut bienvenue dans le jeu de devinette des nombres generer par l'ordinateur\nVeuillez selectioner une des options: " << endl;
-	cout << "\n1. Niveau facile (10 vies)\n2. Intermediaire (5 vies)\n3. Dur (3 vies)\n";
+    cout << "Choix: ";
+    cin >> choix;
 
-	cout << "Choix: " ;
-	cin >> choix;
-	cout << "Devine le nombre: ";
-	cin >> nbrUser;
+    // choix de n avec switch
+    switch (choix) {
+    case 1:
+        vie = 10;
+        break;
+    case 2:
+        vie = 5;
+        break;
+    case 3:
+        vie = 3;
+        break;
+    default:
+        cout << "Choix invalide. Niveau facile selectionne." << endl;
+        vie = 10;
+        break;
+    }
 
+    int vieRestant = vie;
+    bool gagne = false;
 
-	//choix de n avec switch
-	switch (choix) {
-	case 1: vie = 10;
-		break;
-	case 2: vie = 5;
-		break;
-	case 3: vie = 3;
-		break;
-	default:
-		cout << "Niveau facile :" << endl;
-		vie = 10;
-		break;
+    for (int i = 0; i < vie; i++) {
+        cout << "\nDevine le nombre (vie " << i + 1 << "/" << vie << "): ";
+        cin >> nbrUser;
 
-	}
-	int vieRestant = vie;	//initialisation de la vie restante
+        if (nbrUser == nombreDevine) {
+            cout << "Vous avez gagne !!" << endl;
+            gagne = true;
+            break;
+        }
 
-	for (int i = 1; i <=vie; i++) {
+        vieRestant = vie - (i + 1); // compteur de vie 
 
-		if (nbrUser == nombreDevine) {
-			cout << "Vous avez gagner !!";
-			break;
-		}
+        if (nbrUser < nombreDevine) {
+            cout << "Le nombre est plus grand" << endl;
+        }
+        else {
+            cout << "Le nombre est plus petit" << endl;
+        }
 
-		vieRestant = vie - i; //compteur de vie 
+        if (vieRestant > 0) {
+            cout << "Il vous reste : " << vieRestant << " vie(s)" << endl;
+        }
+    }
 
-		if (vieRestant == 0) {
-			cout << "Le nombre etait " << nombreDevine << endl;
-			break;
-		}
+    if (!gagne) {
+        cout << "\nVous avez perdu ! Le nombre etait " << nombreDevine << endl;
+    }
 
-		if(nbrUser!=nombreDevine){
-			
-			if ( nombreDevine>nbrUser ) {
-				cout << "Le nombre est plus grand " << endl;
-			}
-			else {
-				cout << "Le nombre est plus petit " << endl;
-			}
-			cout << "Il vous reste : " << vieRestant << " vie" << endl;
-
-		
-			cout << "Reessayer: ";
-			cin >> nbrUser;
-		}
-		cout << endl;
-
-	}
-	
-
-	return 0;
+    return 0;
 }
